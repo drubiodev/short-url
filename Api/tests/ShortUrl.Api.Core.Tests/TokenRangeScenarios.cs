5796c3b3d97161970b0dca894d7ddc715d7d2d09
Daniel Rubio <drubio432@gmail.com>
2024-11-26 23:00:19 -0500

    add token range test

---
+++ Api/tests/ShortUrl.Api.Core.Tests/TokenRangeScenarios.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using ShortUrl.Core;
+
+namespace ShortUrl.Api.Core.Tests;
+
+public class TokenRangeScenarios
+{
+  [Fact]
+  public void When_start_token_is_greater_than_end_token_then_throws_exception()
+  {
+    var act = () => new TokenRange(10, 5);
+
+    act.Should()
+        .Throw<ArgumentException>()
+        .WithMessage("End must be greater than or equal to start");
+  }
+
+}
